8140e7bfa8561475c58a06f73f3c78591ca5a4ef
jakub <jakubdonovan@protonmail.com>
2021-02-19 15:19:40 +0000

    changed variable names in settings & urls

--- src/settings/components/common.py
+++ src/settings/components/common.py
@@ -44,7 +44,9 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
-            "builtins": [],
+            "builtins": [
+               'django.templatetags.static'
+            ],
         },
     },
 ]
